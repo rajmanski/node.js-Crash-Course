8cf52abd28ad21d01e3451567490668351ea7c65
Piotr Rajmanski <prajmanski10@gmail.com>
2022-08-06 06:08:30 +0200

    File system

---
+++ files.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+//reading files
+fs.readFile('blog1.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log(data.toString());
+});
+
+
+//writting files
+
+fs.writeFile('blog1.txt', 'Hello world!', () => {
+    console.log('File was written!');
+});
+
+fs.writeFile('blog2.txt', 'Blog 2 has been created!', () => {
+    console.log('File was written!');
+})
+
+//directiories
+
+fs.mkdir('assets', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Folder Created');
+});
+
+//removing
+
+fs.rmdir('assets', () => {
+    console.log('Folder deleted');
+});
